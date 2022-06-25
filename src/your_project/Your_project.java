04ff91bb843ad33ea7e0973293ae97041afd03d0
mohhet <qmohit19@gmail.com>
2022-06-25 14:03:38 +0530

    first commit

---
+++ src/your_project/Your_project.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package your_project;
+
+/**
+ *
+ * @author mohit
+ */
+public class Your_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
